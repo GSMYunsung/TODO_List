5e946563ce8a5889aad5febfbeebd4048fb3a5e2
yunsung <12345ggh81@gmail.com>
2021-05-12 21:09:25 +0900

    Tutorial_튜토리얼

--- app/src/main/java/com/example/todolist/Tutorial/Tutorial_MainActivity.kt
+++ app/src/main/java/com/example/todolist/Tutorial/Tutorial_MainActivity.kt
@@ -2,21 +2,29 @@ package com.example.todolist.Tutorial
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.viewpager.widget.ViewPager
+import com.example.todolist.ButtomNavigation_Fragment.*
 import com.example.todolist.MainActivity
 import com.example.todolist.R
+import com.example.todolist.databinding.ActivityMainBinding
 import com.example.todolist.databinding.ActivityTutorialMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import java.util.*
 
-class Tutorial_MainActivity : AppCompatActivity() {
+class Tutorial_MainActivity : AppCompatActivity(){
 
     private lateinit var binding : ActivityTutorialMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_tutorial__main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_tutorial__main)
+
         //setContentView(R.layout.activity_tutorial__main)
+        binding.tutorialBottom.setOnClickListener {
+            binding.mViewPager.setCurrentItem(getItem(+1), true) }
         binding.mViewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
             override fun onPageScrollStateChanged(state: Int) {
             }
@@ -29,13 +37,9 @@ class Tutorial_MainActivity : AppCompatActivity() {
                 when (position) {
                     0 -> {
                         binding.tutorialBottom.setText("NEXT")
-                        binding.tutorialBottom.setOnClickListener {
-                            binding.mViewPager.setCurrentItem(getItem(+1), true) }
                     }
                     1 -> {
                         binding.tutorialBottom.setText("NEXT")
-                        binding.tutorialBottom.setOnClickListener {
-                            binding.mViewPager.setCurrentItem(getItem(+1), true) }
                     }
                     2 -> {
                         binding.tutorialBottom.setText("START")
