5a44758d5852785adc351a190f2e3789e638b160
yunsung <12345ggh81@gmail.com>
2021-05-14 20:23:29 +0900

    MainMenu_

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -5,7 +5,9 @@ import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.SnapHelper
 import com.example.todolist.ButtomNavigation_Fragment.*
 import com.example.todolist.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -55,6 +57,8 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
 
     private var listview: RecyclerView? = null
     private lateinit var binding2 : ActivityMainBinding
+    val snapHelper: SnapHelper = PagerSnapHelper()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding2 = DataBindingUtil.setContentView(this, R.layout.activity_main)
@@ -64,6 +68,19 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
 
 //        //오늘의 todolist 보여지게하는 어뎁터와 네비게이션 불러오기
 //        binding2.recyclerView.layoutManager = LinearLayoutManager(this)
-        binding2.navigation.setOnNavigationItemSelectedListener(this)
+          binding2.navigation.setOnNavigationItemSelectedListener(this)
+
+        //리사이클러뷰의 속도가 null일경우 즉, 움직이지 않을경우 정의
+        //움직이지 않을경우 다음 item으로 넘어감을 의미 따라서 다시 리사이클러뷰 변동사항 저장
+        if (binding2.recyclerView.getOnFlingListener() == null)
+            snapHelper.attachToRecyclerView(binding2.recyclerView)
+
+
+        //recyclerView의 마지막 포지션을 반환해주어 체크하고 멈추게 한다.
+        binding2.recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+            }
+        })
     }
 }
