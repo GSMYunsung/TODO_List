3564d2a5bba68d0b9b52b81ac6ac37cb37ad3581
yunsung <12345ggh81@gmail.com>
2021-05-18 15:42:18 +0900

    Today_todo_나의 오늘

--- app/src/main/java/com/example/todolist/ButtomNavigation_Fragment/Today_todo_Fragment.kt
+++ app/src/main/java/com/example/todolist/ButtomNavigation_Fragment/Today_todo_Fragment.kt
@@ -1,6 +1,9 @@
 package com.example.todolist.ButtomNavigation_Fragment
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -8,11 +11,29 @@ import com.example.todolist.*
 
 class Today_todo_Fragment : Fragment() {
 
+
+    //onCreateView로 View를 만들어주어야한다!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_today_todo,container,false)//프레그먼트 레이아웃 받아오는 부분
+
+        view.findViewById<RecyclerView>(R.id.today_todo_recycler_view_imsy).adapter = Today_todoAdapter()
+        view.findViewById<RecyclerView>(R.id.today_todo_recycler_view_imsy).layoutManager = LinearLayoutManager(activity)
+
+
+        return view
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        variable.binding2.todayTodoRecyclerView.layoutManager = LinearLayoutManager(requireContext())
-        variable.binding2.todayTodoRecyclerView.adapter = Today_todoAdapter()
+
+        view?.findViewById<RecyclerView>(R.id.today_todo_recycler_view_imsy)?.layoutManager = LinearLayoutManager(requireContext())
+        view?.findViewById<RecyclerView>(R.id.today_todo_recycler_view_imsy)?.adapter = Today_todoAdapter()
+//
+//        variable.binding2.todayTodoRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+//        variable.binding2.todayTodoRecyclerView.adapter = Today_todoAdapter()
 
     }
 }
